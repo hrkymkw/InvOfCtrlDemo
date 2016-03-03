be0569da62500750956c405579081628aea35ad2
James Swindell <swintwin@gmail.com>
2016-03-03 17:05:22 -0500

    demostrates a quick and dirty way to setup and use Ninject's IoC tooling

---
+++ InvOfCtrlDemo/ITestService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvOfCtrlDemo
+{
+    interface ITestService
+    {
+        string[] GetMeSomeNames();
+    }
+
+    class TestService : ITestService
+    {
+        public string[] GetMeSomeNames()
+        {
+            return new[] { "Chris", "Hiro", "James", "Purvin", "Stan", "Steve" };
+        }
+    }
+}
---
+++ InvOfCtrlDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace InvOfCtrlDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // setup up a new standard "container"
+            var kernel = new Ninject.StandardKernel();
+
+            kernel.Bind<ITestService>().To<TestService>();
+
+            // calls for a server based on a registered type
+            var service = kernel.Get<ITestService>();
+
+            // work with your resolved service
+            var names = service.GetMeSomeNames();
+            foreach(var name in names)
+            {
+                Console.WriteLine($"My name is {name}");
+            }
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+    }
+}
